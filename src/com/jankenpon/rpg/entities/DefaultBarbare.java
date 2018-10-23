fcf89113581f2633260758f2cf4a0c9a2f825f51
NicolasG <nicolas.grihault@imie.fr>
2018-10-23 13:48:04 +0200

    getter+setter+construteurOK

--- src/com/jankenpon/rpg/entities/DefaultBarbare.java
+++ src/com/jankenpon/rpg/entities/DefaultBarbare.java
@@ -2,4 +2,40 @@ package com.jankenpon.rpg.entities;
 
 public class DefaultBarbare extends DefaultFighter implements Barbare {
 
+	@Override
+	public String getArmorRestriction() {
+		// TODO Auto-generated method stub
+		return super.getArmorRestriction();
+	}
+
+	@Override
+	public String getWeaponRestriction() {
+		// TODO Auto-generated method stub
+		return super.getWeaponRestriction();
+	}
+
+	@Override
+	public void fight(Personnage defender) {
+		// TODO Auto-generated method stub
+		super.fight(defender);
+	}
+
+	@Override
+	public boolean isEquipable(Arme arme) {
+		// TODO Auto-generated method stub
+		if (arme instanceof ArmePhysique) { 
+			return true;
+		}
+		return super.isEquipable(arme);
+	}
+
+	@Override
+	public boolean isEquipable(Armure armure) {
+		// TODO Auto-generated method stub
+		if (armure instanceof ArmurePhysique) { 
+			return true;
+		}
+		return super.isEquipable(armure);
+	}
+
 }
--- src/com/jankenpon/rpg/entities/DefaultMagicien.java
+++ src/com/jankenpon/rpg/entities/DefaultMagicien.java
@@ -2,5 +2,40 @@ package com.jankenpon.rpg.entities;
 
 public class DefaultMagicien extends DefaultFighter implements Magicien {
 
+	@Override
+	public String getArmorRestriction() {
+		// TODO Auto-generated method stub
+		return super.getArmorRestriction();
+	}
+
+	@Override
+	public String getWeaponRestriction() {
+		// TODO Auto-generated method stub
+		return super.getWeaponRestriction();
+	}
+
+	@Override
+	public void fight(Personnage defender) {
+		// TODO Auto-generated method stub
+		super.fight(defender);
+	}
+
+	@Override
+	public boolean isEquipable(Arme arme) {
+		if (arme instanceof ArmeMagique) { 
+			return true;
+		}
+		return super.isEquipable(arme);
+	}
+
+	@Override
+	public boolean isEquipable(Armure armure) {
+		// TODO Auto-generated method stub
+		if (armure instanceof ArmureMagique) { 
+			return true;
+		}
+		return super.isEquipable(armure);
+	}
+
 }
 
